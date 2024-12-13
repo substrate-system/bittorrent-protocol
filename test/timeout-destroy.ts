ed16da389735f341580ff8072d4e96d7a2e8dbb2
nichoth <nichoth@nichoth.com>
2024-12-12 23:42:33 -0800

    another test passes

--- test/timeout-destroy.ts
+++ test/timeout-destroy.ts
@@ -1,35 +1,49 @@
-import test from 'tape'
-import Protocol from '../index.js'
+import { test } from '@substrate-system/tapzero'
+import Protocol from '../src/index.js'
 
-test('Timeout and destroy when peer does not respond', t => {
+test('Timeout and destroy when peer does not respond', async t => {
     t.plan(4)
 
     let timeouts = 0
+    const wire = await Protocol.create()
 
-    const wire = new Protocol()
-    wire.on('error', err => { t.fail(err) })
-    wire.pipe(wire)
-    wire.setTimeout(1000)
-    wire.handshake(Buffer.from('01234567890123456789'), Buffer.from('12345678901234567890'))
+    return new Promise<void>(resolve => {
+        let n = 0
+        wire.on('error', err => t.fail(err))
+        wire.pipe(wire)
+        wire.setTimeout(1000)
+        wire.handshake(
+            Buffer.from('01234567890123456789'),
+            Buffer.from('12345678901234567890')
+        )
 
-    wire.on('unchoke', () => {
-        wire.request(0, 0, 0, err => {
-            t.ok(err)
+        wire.on('unchoke', () => {
+            wire.request(0, 0, 0, err => {
+                t.ok(err)
+                n++
+                if (n === 4) resolve()
+            })
+
+            wire.request(0, 0, 0, err => {
+                t.ok(err)
+                n++
+                if (n === 4) resolve()
+            })
+
+            wire.request(0, 0, 0, err => {
+                t.ok(err)
+                n++
+                if (n === 4) resolve()
+            })
         })
 
-        wire.request(0, 0, 0, err => {
-            t.ok(err)
+        wire.on('timeout', () => {
+            t.equal(++timeouts, 1)
+            wire.end()
+            n++
+            if (n === 4) resolve()
         })
 
-        wire.request(0, 0, 0, err => {
-            t.ok(err)
-        })
+        wire.unchoke()
     })
-
-    wire.on('timeout', () => {
-        t.equal(++timeouts, 1)
-        wire.end()
-    })
-
-    wire.unchoke()
 })
--- test/timeout.ts
+++ test/timeout.ts
@@ -1,39 +1,53 @@
-import test from 'tape'
-import Protocol from '../index.js'
+import { test } from '@substrate-system/tapzero'
+import Protocol from '../src/index.js'
 
-test('Timeout when peer does not respond', t => {
+test('Timeout when peer does not respond', async t => {
     t.plan(9)
 
     let timeouts = 0
+    const wire = await Protocol.create()
 
-    const wire = new Protocol()
-    wire.on('error', err => { t.fail(err) })
-    wire.pipe(wire)
-    wire.setTimeout(1000)
-    wire.handshake(Buffer.from('01234567890123456789'), Buffer.from('12345678901234567890'))
+    return new Promise<void>(resolve => {
+        let n = 0
+        wire.on('error', err => t.fail(err))
+        wire.pipe(wire)
+        wire.setTimeout(1000)
+        wire.handshake(
+            Buffer.from('01234567890123456789'),
+            Buffer.from('12345678901234567890')
+        )
 
-    wire.on('unchoke', () => {
-        let requests = 0
+        wire.on('unchoke', () => {
+            let requests = 0
 
-        wire.request(0, 0, 0, err => {
-            t.ok(err)
-            t.ok(++requests === 1)
+            wire.request(0, 0, 0, err => {
+                t.ok(err)
+                t.ok(++requests === 1)
+                n += 2
+                if (n === 9) resolve()
+            })
+
+            wire.request(0, 0, 0, err => {
+                t.ok(err)
+                t.ok(++requests === 2)
+                n += 2
+                if (n === 9) resolve()
+            })
+
+            wire.request(0, 0, 0, err => {
+                t.ok(err)
+                t.ok(++requests === 3)
+                n += 2
+                if (n === 9) resolve()
+            })
         })
 
-        wire.request(0, 0, 0, err => {
-            t.ok(err)
-            t.ok(++requests === 2)
+        wire.on('timeout', () => {
+            t.ok(++timeouts <= 3) // should get called 3 times
+            n++
+            if (n === 9) resolve()
         })
 
-        wire.request(0, 0, 0, err => {
-            t.ok(err)
-            t.ok(++requests === 3)
-        })
+        wire.unchoke()
     })
-
-    wire.on('timeout', () => {
-        t.ok(++timeouts <= 3) // should get called 3 times
-    })
-
-    wire.unchoke()
 })
