4ef15d97b977e3f3d3f352927a3ec9f3666e0ab4
nichoth <nichoth@nichoth.com>
2024-12-16 20:34:39 -0800

    better build script

--- build.js
+++ build.js
@@ -1,17 +1,13 @@
 import { build } from 'esbuild'
 import { polyfillNode } from '@substrate-system/esbuild-plugin-polyfill-node'
 
-// "build-cjs": "esbuild src/*.ts --format=cjs --keep-names --tsconfig=tsconfig.build.json --outdir=./dist --out-extension:.js=.cjs --sourcemap",
-// "build-cjs:min": "esbuild src/*.ts --format=cjs --minify --keep-names --tsconfig=tsconfig.build.json --outdir=./dist --out-extension:.js=.min.cjs --sourcemap",
-// "build-esm": "esbuild src/*.ts --format=esm --metafile=dist/meta.json --keep-names --tsconfig=tsconfig.build.json --outdir=./dist --sourcemap && tsc --emitDeclarationOnly --project tsconfig.build.json --outDir dist",
-// "build-esm:min": "esbuild ./src/*.ts --format=esm --keep-names --bundle --tsconfig=tsconfig.build.json --minify --out-extension:.js=.min.js --outdir=./dist --sourcemap",
-
-// esm
+// ESM
 build({
     entryPoints: ['src/*.ts'],
     bundle: false,
     format: 'esm',
     keepNames: true,
+    metafile: true,
     tsconfig: './tsconfig.build.json',
     outdir: './dist',
     sourcemap: true,
@@ -24,18 +20,65 @@ build({
     ],
 })
 
-// // bundle
-// build({
-//     entryPoints: ['src/*.ts'],
-//     bundle: true,
-//     format: 'esm',
-//     keepNames: true,
-//     tsconfig: './tsconfig.build.json',
-//     outdir: './dist',
-//     sourcemap: true,
-//     plugins: [
-//         polyfillNode({
-//             // Options (optional)
-//         }),
-//     ],
-// })
+// ESM + minified
+build({
+    entryPoints: ['src/*.ts'],
+    format: 'esm',
+    bundle: true,
+    minify: true,
+    keepNames: true,
+    tsconfig: './tsconfig.build.json',
+    outdir: './dist',
+    plugins: [
+        polyfillNode({
+            polyfills: {
+                crypto: true
+            }
+        }),
+    ],
+    outExtension: {
+        '.js': '.min.js'
+    }
+})
+
+// CJS
+build({
+    entryPoints: ['src/*.ts'],
+    format: 'cjs',
+    bundle: false,
+    minify: false,
+    keepNames: true,
+    tsconfig: './tsconfig.build.json',
+    outdir: './dist',
+    plugins: [
+        polyfillNode({
+            polyfills: {
+                crypto: true
+            }
+        }),
+    ],
+    outExtension: {
+        '.js': '.cjs'
+    }
+})
+
+// CJS + minified
+build({
+    entryPoints: ['src/*.ts'],
+    format: 'cjs',
+    bundle: true,
+    minify: true,
+    keepNames: true,
+    tsconfig: './tsconfig.build.json',
+    outdir: './dist',
+    plugins: [
+        polyfillNode({
+            polyfills: {
+                crypto: true
+            }
+        }),
+    ],
+    outExtension: {
+        '.js': '.min.cjs'
+    }
+})
